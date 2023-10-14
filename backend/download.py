5b8122ef8c756563dbf657173aeecb7537176187
genkerizer <nguyenyhop1999@gmail.com>
2023-10-14 19:16:07 +0700

    add download module

--- backend/download.py
+++ backend/download.py
@@ -1,5 +1,3 @@
 import gdown
-
-url = 'https://drive.google.com/file/d/1Plofi09JZ6UOQH4WIfpnF2Cz9nyXbocw/view?usp=sharing'
-output_path = 'module.zip'
-gdown.download(url, output_path, quiet=False,fuzzy=True)
+url = 'https://drive.google.com/drive/folders/18Vz7_VAyTmpkz0d6qWnRxHQQMJJHq6j5?usp=sharing'
+gdown.download_folder(url, quiet=True, use_cookies=False)
