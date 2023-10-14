0a99c4ff8912a12d900b43ea5a6033291b0f1601
genkerizer <nguyenyhop1999@gmail.com>
2023-10-14 19:40:32 +0700

    add download from drive

--- backend/download.py
+++ backend/download.py
@@ -1,3 +1,4 @@
 import gdown
 url = 'https://drive.google.com/drive/folders/18Vz7_VAyTmpkz0d6qWnRxHQQMJJHq6j5?usp=sharing'
-gdown.download_folder(url, quiet=True, use_cookies=False)
+gdown.download_folder(url, quiet=False, use_cookies=False)
+
